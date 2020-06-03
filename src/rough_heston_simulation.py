095465c338f7de881e82c549f2f411fa1e82fe88
Aleksandr Tukallo <alex.tukallo@gmail.com>
2020-06-03 18:55:17 +0300

    move rough heston calculation to .py file

---
+++ src/rough_heston_simulation.py
@@ -0,0 +1,104 @@
+import numpy as np
+from scipy import signal
+
+import math
+
+import matplotlib.pyplot as plt
+
+"""
+The module implements path simulation for rough Heston model
+
+Usual Heston model has the following form (interest rate and dividend rate are assumed to be zero):
+d X_t = - V_t / 2 * dt + sqrt(V_t) * d W_t
+V_t = Y_t
+d Y_t = (theta - kappa * Y_t) * dt + sigma * sqrt(Y_t) * d B_t
+[W_t, B_t] = rho
+The model parameters are {theta, kappa, sigma, rho}, observables are {X_0, Y_0}.
+Under such log price movement stock price follows
+d S_t = sqrt(Y_t) * S_t * d W_t
+For reference see https://nbviewer.jupyter.org/url/people.math.ethz.ch/~jteichma/lecture_ml_web/heston_calibration.ipynb
+
+Rough Heston model is (interest rate and dividend rate are assumed to be zero):
+d X_t = - V_t / 2 * dt + sqrt(V_t) * d W_t
+V_t = (G^alpha Y)_t
+d Y_t = (theta - kappa * Y_t) * dt + sigma * sqrt(Y_t) * d B_t
+where G^alpha is the Generalized Fractional Operator with alpha in (-1/2, 1/2).
+G^alpha can be understood as an operator, which transforms the Hölder-1/2 function to Hölder-(1/2+alpha) function
+and thus makes it 'rougher'.
+To model parameters one more parameter {alpha} is added.
+For reference see "Functional Central Limit Theorems for Rough Volatility" by Horvath et al and "Asymptotic 
+Behaviour of the Fractional Heston Model" by Shi et al.
+
+Initial version implemented by mgrillo, then refactored and standardized by atukallo
+"""
+
+
+def simulate_rough_heston(n, m, terminal_time=1, log_spot_price=1, inst_var=0.1, mean_rev_speed=1, eq_var=0.1,
+                          vol_of_vol=0.01, correlation=0.1, alpha=0.):
+    """
+    todo: comment
+    n is number of time steps
+    m is number of simulations
+    """
+    T = terminal_time
+    X_0 = log_spot_price
+    Y_0 = inst_var
+    kappa = mean_rev_speed
+    theta = eq_var
+    sigma = vol_of_vol
+    rho = correlation
+
+    #todo: what should be assert on parameters to guarantee V > 0 ?
+
+    Y, B = __simulate_volatility(n, m, T, Y_0, kappa, theta, sigma)
+    V = __simulate_rough_volatility(n, m, T, alpha, Y)
+    X = __simulate_log_price(n, m, T, X_0, rho, V, B)
+
+    return X
+
+
+def __simulate_volatility(n, m, T, Y_0, kappa, theta, sigma):
+    """
+    Simulates volatility process Y_t and returns it together with its brownian motion W_t
+    """
+    sqrtn = np.sqrt(n)
+    vola = np.zeros((n, m))
+    B = np.random.normal(0.0, 1.0, size=(n, m))
+    vola[0] = Y_0
+    for i in range(1, n):
+        # todo(atukallo): why T / sqrtn ?
+        vola[i] = vola[i - 1] \
+                  + (theta - kappa * vola[i - 1]) * (T / n) \
+                  + (sigma * np.sqrt(vola[i - 1])) * (T / sqrtn) * B[i - 1]
+    return vola, B
+
+
+def __simulate_rough_volatility(n, m, T, alpha, Y):
+    V = np.zeros((n, m))
+    g_vector = np.array([np.power(1. * T * i / n, alpha) for i in range(1, n)])  # dropping 0th element
+    vola_incrs = Y[1:] - Y[:-1]
+
+    V[0] = Y[0]
+    # todo(mgrillo,atukallo): introduce convolution, which is vectorized over m
+    for i in range(m):
+        # documentation recommends using fft only for big arrays
+        if n > 500:
+            V[1:, i] = signal.fftconvolve(g_vector, vola_incrs[:, i], 'same')
+        else:
+            V[1:, i] = signal.convolve(g_vector, vola_incrs[:, i], 'same')
+    return V
+
+
+def __simulate_log_price(n, m, T, X_0, rho, V, B):
+    assert not np.any(np.isnan(V) | np.isnan(B))
+    assert np.nanmin(V) > 0
+
+    X = np.zeros((n, m))
+    W = rho * B + math.sqrt(1 - np.power(rho, 2)) * np.random.normal(0.0, 1.0, size=(n, m))
+    X[0] = X_0
+    # todo(mgrillo,atukallo): code version is very different from the initial one, which is correct?
+    for i in range(1, n):
+        X[i] = X[i - 1] \
+               - 0.5 * (T / n) * V[i - 1] \
+               + np.sqrt(V[i - 1]) * np.sqrt(T / n) * W[i - 1]
+    return X
