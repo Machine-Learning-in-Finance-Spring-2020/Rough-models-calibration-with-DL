293502adbb380b36ae7ce34d8773260a960e38e7
Aleksandr Tukallo <alex.tukallo@gmail.com>
2020-06-03 22:23:38 +0300

    add rough SABR model to .py file

--- src/rough_simulations.py
+++ src/rough_simulations.py
@@ -1,18 +1,22 @@
+"""
+The module implements path simulation for few models with rough volatility including rough Heston model
+and rough SABR model. The main reference paper is "Functional Central Limit Theorems for Rough Volatility"
+by Horvath et al.
+
+Initial version of the code implemented by mgrillo, then refactored and standardized by atukallo
+"""
 import numpy as np
+import functools
+
 from scipy import signal
 
-import math
-
-import matplotlib.pyplot as plt
-
 """
-The module implements path simulation for rough Heston model
-
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Heston ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Usual Heston model has the following form (interest rate and dividend rate are assumed to be zero):
 d X_t = - V_t / 2 * dt + sqrt(V_t) * d W_t
 V_t = Y_t
 d Y_t = (theta - kappa * Y_t) * dt + sigma * sqrt(Y_t) * d B_t
-[W_t, B_t] = rho
+d[W_t, B_t] = rho * dt
 The model parameters are {theta, kappa, sigma, rho}, observables are {X_0, Y_0}.
 Under such log price movement stock price follows
 d S_t = sqrt(Y_t) * S_t * d W_t
@@ -22,21 +26,19 @@ Rough Heston model is (interest rate and dividend rate are assumed to be zero):
 d X_t = - V_t / 2 * dt + sqrt(V_t) * d W_t
 V_t = (G^alpha Y)_t
 d Y_t = (theta - kappa * Y_t) * dt + sigma * sqrt(Y_t) * d B_t
+d[W_t, B_t] = rho * dt
 where G^alpha is the Generalized Fractional Operator with alpha in (-1/2, 1/2).
 G^alpha can be understood as an operator, which transforms the Hölder-1/2 function to Hölder-(1/2+alpha) function
 and thus makes it 'rougher'.
 To model parameters one more parameter {alpha} is added.
 For reference see "Functional Central Limit Theorems for Rough Volatility" by Horvath et al and "Asymptotic 
 Behaviour of the Fractional Heston Model" by Shi et al.
-
-Initial version implemented by mgrillo, then refactored and standardized by atukallo
 """
 
 
 def simulate_rough_heston(n, m, terminal_time=1, log_spot_price=1, inst_var=0.1, mean_rev_speed=1, eq_var=0.1,
                           vol_of_vol=0.01, correlation=0.1, alpha=0.):
     """
-    todo: comment
     n is number of time steps
     m is number of simulations
     """
@@ -48,16 +50,18 @@ def simulate_rough_heston(n, m, terminal_time=1, log_spot_price=1, inst_var=0.1,
     sigma = vol_of_vol
     rho = correlation
 
-    #todo: what should be assert on parameters to guarantee V > 0 ?
+    # todo(mgrillo,atukallo): what should be assert on parameters to guarantee V > 0 ? Should further debug, not
+    #   production ready yet.
 
-    Y, B = __simulate_volatility(n, m, T, Y_0, kappa, theta, sigma)
+    Y, B = __simulate_heston_volatility(n, m, T, Y_0, kappa, theta, sigma)
     V = __simulate_rough_volatility(n, m, T, alpha, Y)
-    X = __simulate_log_price(n, m, T, X_0, rho, V, B)
+    W = __get_correlated_bm(rho, B)
+    X = __simulate_heston_log_price(n, m, T, X_0, rho, V, W)
 
     return X
 
 
-def __simulate_volatility(n, m, T, Y_0, kappa, theta, sigma):
+def __simulate_heston_volatility(n, m, T, Y_0, kappa, theta, sigma):
     """
     Simulates volatility process Y_t and returns it together with its brownian motion W_t
     """
@@ -89,16 +93,75 @@ def __simulate_rough_volatility(n, m, T, alpha, Y):
     return V
 
 
-def __simulate_log_price(n, m, T, X_0, rho, V, B):
-    assert not np.any(np.isnan(V) | np.isnan(B))
+def __simulate_heston_log_price(n, m, T, X_0, rho, V, W):
+    assert not np.any(np.isnan(V) | np.isnan(W))
     assert np.nanmin(V) > 0
 
     X = np.zeros((n, m))
-    W = rho * B + math.sqrt(1 - np.power(rho, 2)) * np.random.normal(0.0, 1.0, size=(n, m))
     X[0] = X_0
     # todo(mgrillo,atukallo): code version is very different from the initial one, which is correct?
     for i in range(1, n):
         X[i] = X[i - 1] \
-               - 0.5 * (T / n) * V[i - 1] \
+               - 0.5 * V[i - 1] * (T / n) \
                + np.sqrt(V[i - 1]) * np.sqrt(T / n) * W[i - 1]
+    return X
+
+
+def __get_correlated_bm(rho, B):
+    W = rho * B + np.sqrt(1 - np.power(rho, 2)) * np.random.normal(0.0, 1.0, size=B.shape)
+    return W
+
+
+"""
+Rough SABR model has the following form for log-price:
+d X_t = V_t * L(t, X_t) * d W_t - 0.5 * (V_t)^2 * L(t, X_t)^2 * dt
+V_t = (G^alpha Y)_t
+d Y_t = sigma * Y_t * d B_t
+d[W_t, B_t] = rho * dt
+Thus SABR model combines rough stochastic and local volatility
+"""
+
+
+def simulate_rough_SABR(n, m, terminal_time=1, log_spot_price=1, inst_var=0.1,
+                        vol_of_vol=0.01, correlation=0.1, local_vola=lambda t, log_price: 1, alpha=0.):
+    T = terminal_time
+    X_0 = log_spot_price
+    Y_0 = inst_var
+    sigma = vol_of_vol
+    rho = correlation
+    L = local_vola
+
+    Y, B = __simulate_SABR_volatility(n, m, T, Y_0, sigma)
+    V = __simulate_rough_volatility(n, m, T, alpha, Y)
+    W = __get_correlated_bm(rho, B)
+    X = __simulate_SABR_log_price(n, m, T, X_0, rho, L, V, W)
+
+    return X
+
+
+def __simulate_SABR_volatility(n, m, T, Y_0, sigma):
+    """
+    Simulates volatility process Y_t and returns it together with its brownian motion W_t
+    """
+    sqrtn = np.sqrt(n)
+    vola = np.zeros((n, m))
+    B = np.random.normal(0.0, 1.0, size=(n, m))
+    vola[0] = Y_0
+    for i in range(1, n):
+        # todo(atukallo): why T / sqrtn ?
+        vola[i] = vola[i - 1] \
+                  + (sigma * np.sqrt(vola[i - 1])) * (T / sqrtn) * B[i - 1]
+    return vola, B
+
+
+def __simulate_SABR_log_price(n, m, T, X_0, rho, L, V, W):
+    assert not np.any(np.isnan(V) | np.isnan(B))
+
+    X = np.zeros((n, m))
+    X[0] = X_0
+    for i in range(1, n):
+        binded_L = np.vectorize(functools.partial(L, T * (i - 1) / n))
+        X[i] = X[i - 1] \
+               - 0.5 * np.pow(V[i - 1], 2) * np.pow(binded_L(X[i - 1]), 2) * (T / n) \
+               + V[i - 1] * np.sqrt(T / n) * W[i - 1]
     return X
